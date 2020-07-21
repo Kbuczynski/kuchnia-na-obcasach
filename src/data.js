bbd6e329a37d619f677c5e088b1f3e1f607dc33a
Kbuczynski <kamil.buczynski00@interia.pl>
2020-07-21 17:19:14 +0200

    changed API url

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const API = `http://kuchnianaobcasach.atthost24.pl/wp-json/wp/v2/`;
+export const API = `https://kuchnianaobcasach.atthost24.pl/wp-json/wp/v2/`;
 
 export const POSTS_PER_PAGE = 12;
 
