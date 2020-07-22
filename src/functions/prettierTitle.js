aa5c3c2caf349631267dbb8edf434c7d1edca2fc
Kbuczynski <kamil.buczynski00@interia.pl>
2020-07-23 01:36:30 +0200

    fixed post content image

--- src/functions/prettierTitle.js
+++ src/functions/prettierTitle.js
@@ -3,6 +3,7 @@ const prettierTitle = (title) => {
 
     if (title.indexOf("&#8220;") !== -1) title = title.replace("&#8220;", `"`);
     if (title.indexOf("&#8221;") !== -1) title = title.replace("&#8221;", `"`);
+    if (title.indexOf("&#8211;") !== -1) title = title.replace("&#8211;", `-`);
     
     return `${title[0].toUpperCase()}${title.slice(1)}`; 
 }
