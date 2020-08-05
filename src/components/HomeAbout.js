8e4ce6a6b71cb8ce32bb7eae0854a0128b647c92
Kbuczynski <kamil.buczynski00@interia.pl>
2020-08-05 20:30:21 +0200

    change about content

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -16,15 +16,19 @@ const HomeAbout = () => {
         <h1>Kuchnia na obcasach</h1>
       </div>
       <p className="home__about__text">
-        Kuchnia na Obcasach to blog kulinarny z przepisami na smaczne, zdrowie i
-        sezonowe potrawy. Znajdziecie tu zbiór na klasyczne dania, przepisy z
-        nutką nowoczesności, a także potrawy dietetyczne, dla osób zdrowo
-        odżywiających się i będących fit, a osoby, które dopiero zaczynają swoją
-        przygodę z gotowaniem na pewno znajdą tutaj wiele ciekawych inspiracji i
+        Kuchnia na Obcasach to blog kulinarny z przepisami na smaczne, zdrowe i
+        sezonowe potrawy. Znajdziecie tu przepisy na dania klasyczne, z nutką
+        nowoczesności, a także potrawy dietetyczne dla osób zdrowo odżywiających
+        się i będących fit wraz z podaną kalorycznością i rozkładem makro
+        składników. Znajdziecie tu także sporą dawkę inspiracji na słodkie
+        wypieki, desery i fit słodkości. Osoby, które dopiero zaczynają swoją
+        przygodę z gotowaniem na pewno znajdą tutaj wiele cennych wskazówek i
         porad kulinarnych. Serdecznie zapraszam do korzystania i próbowania
         przepisów.
       </p>
-      <button className="home__about__button" onClick={handleScroll}>Znajdź coś dla siebie!</button>
+      <button className="home__about__button" onClick={handleScroll}>
+        Znajdź coś dla siebie!
+      </button>
     </>
   );
 };
