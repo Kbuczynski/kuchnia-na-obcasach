c0e3fca556a6fabc9c3fd383a648a14eb16008d9
Kbuczynski <kamil.buczynski00@interia.pl>
2020-09-20 14:09:01 +0200

    change title format

--- src/functions/prettierTitle.js
+++ src/functions/prettierTitle.js
@@ -1,11 +1,9 @@
 const prettierTitle = (title) => {
-    title = title.toLowerCase();
-
     if (title.indexOf("&#8220;") !== -1) title = title.replace(/&#8220;/g, `"`);
     if (title.indexOf("&#8221;") !== -1) title = title.replace(/&#8221;/g, `"`);
     if (title.indexOf("&#8211;") !== -1) title = title.replace(/&#8211;/g, `-`);
     
-    return `${title[0].toUpperCase()}${title.slice(1)}`; 
+    return title;
 }
  
 export default prettierTitle;
